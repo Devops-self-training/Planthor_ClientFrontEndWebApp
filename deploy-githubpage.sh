464c3e290b07b48416dc5e7cc8a90a9c40cc4278
akakshuki <giangbaccai1207@gmail.com>
2021-05-22 19:58:46 +0700

    add command

---
+++ deploy-githubpage.sh
@@ -0,0 +1,6 @@
+git init
+git add .
+git commit -m "update github frontend"
+git branch -M main
+git remote add origin https://akakshuki:Dinhhoang1207@github.com/Devops-self-training/DemoFronendt12879.github.io.git
+git push -u origin main
